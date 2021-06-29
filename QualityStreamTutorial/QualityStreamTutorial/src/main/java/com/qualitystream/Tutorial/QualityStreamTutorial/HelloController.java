e4762159ab32e5598a6037450944be61a64ce5df
Joaquin Orue <joaquin.orue@gmail.com>
2021-06-29 16:16:04 -0300

    Primer Commit

---
+++ QualityStreamTutorial/QualityStreamTutorial/src/main/java/com/qualitystream/Tutorial/QualityStreamTutorial/HelloController.java
@@ -0,0 +1,18 @@
+package com.qualitystream.Tutorial.QualityStreamTutorial;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
---
+++ QualityStreamTutorial/QualityStreamTutorial/src/main/java/com/qualitystream/Tutorial/QualityStreamTutorial/QualityStreamTutorialRestApplication.java
@@ -0,0 +1,13 @@
+package com.qualitystream.Tutorial.QualityStreamTutorial;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class QualityStreamTutorialRestApplication extends Application {
+}
